a3a8e332ea9102476164455a61ac28fa734e7718
Renan Lucilio <renanlucilio@gmail.com>
2021-12-14 12:08:09 -0300

    first commit

---
+++ src/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>nome</h1>
+    <input type="text" v-model="nome" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hello-component",
+  data: () => ({ nome: "Renan" }),
+};
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
